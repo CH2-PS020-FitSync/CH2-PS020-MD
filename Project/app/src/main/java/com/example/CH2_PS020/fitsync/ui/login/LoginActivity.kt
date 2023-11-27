05313a4c341aef03395e21e6115572ec33ce54d6
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2023-11-27 23:43:49 +0700

    ADD: -ui LoginActivity RegisterActivity
    Finishing LoginActivity XML and Theming, Up Next Register

---
+++ Project/app/src/main/java/com/example/CH2_PS020/fitsync/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.CH2_PS020.fitsync.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.CH2_PS020.fitsync.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
