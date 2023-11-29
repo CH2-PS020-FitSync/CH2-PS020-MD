3e9ce9dad74a777fe8870d9968d41cb9bc83d5d1
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2023-11-29 14:55:38 +0700

    ADD: -ui FLow of Forget Password, Dialog Welcome
    Completely designing layout of flow forget password, dialog welcome.

--- Project/app/build.gradle.kts
+++ Project/app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
 
     //Test
     testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ Project/app/src/main/java/com/example/CH2_PS020/fitsync/ui/forgotpassword/CreatePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.CH2_PS020.fitsync.ui.forgotpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.CH2_PS020.fitsync.R
+
+class CreatePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_password)
+    }
+}
